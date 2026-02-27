90c636d4152a4ff290afeafaf088663b82092f38
Maria <79464091+mar441@users.noreply.github.com>
2026-02-27 17:14:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -338,5 +338,6 @@ def display_displacement(clickData):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8060))
+    app.run_server(host="0.0.0.0", port=port, debug=False)
